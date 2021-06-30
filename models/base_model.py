21db1e28d190f471ea5d1907d3c9e9f5691da76f
Rode1o <dumirola.dr@gmail.com>
2021-06-29 20:04:45 -0500

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -23,18 +23,17 @@ class BaseModel():
         """Constructor"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                #elif key == "updated_at":
+                 #   self.updated_at = datetime.strptime(value,
+                  #                                      "%Y-%m-%dT%H:%M:%S.%f")
+                if key not in ["__class__"]:
                     setattr(self, key, value)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """print method"""
